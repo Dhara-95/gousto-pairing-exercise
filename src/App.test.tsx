1b9ec5ea7388410c829a7851fd62634519ec0aa8
Dhara Patel <dhara.patel95@hotmail.co.uk>
2022-03-09 14:42:37 +0000

    revert tests to default set up

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,1 @@
-import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import { App } from "./App";
-
-describe("App", () => {
-  it("renders the App header", () => {
-    render(<App />);
-    const header = screen.getByText("Gousto Pairing Exercise");
-
-    expect(header).toBeInTheDocument();
-  });
-});
